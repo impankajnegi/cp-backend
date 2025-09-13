9c6945bd182591149bdf052353678a901b26fa10
Pankaj Negi <pnegi.dun@gmail.com>
2025-09-13 13:04:13 +0530

    created express root app file

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import userRoutes from "./modules/user/user.routes";  
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/users", userRoutes); 
+ 
+export default app;
